c707954710d9865b349eefe6235da8e89d943a31
Felix Wang <wangfelix87@gmail.com>
2016-12-25 15:13:51 +0800

    comment unused imports

--- hezila.go
+++ hezila.go
@@ -1,22 +1,22 @@
 package hezila
 
 import (
-	"net/http"
-	"sync"
+	//"net/http"
+	//"sync"
 
 	"github.com/hezila/hezila/cache"
 )
 
 // Hezila
 type Hezila struct {
-	
 }
 
 // Config for user
 type Config struct {
 	Cache cache.Cache
 }
+
 // NewHezila init
 func NewHezila(cfg *Config) *Hezila {
-	
+
 }
