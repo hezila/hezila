0313451cad20a647cd85de2dc9351f1900ae868b
felix <wangfelix87@gmail.com>
2016-12-29 21:27:48 +0800

    minor revision

--- utils/test.go
+++ utils/test.go
@@ -9,12 +9,12 @@ import (
 func Expect(t *testing.T, expect string, actual interface{}) {
 	actualString := fmt.Sprint(actual)
 	if expect != actualString {
-		t.Errorf("期待值=\"%s\", 实际=\"%s\"", expect, actualString)
+		t.Errorf("Expeted value=\"%s\", Actual value=\"%s\"", expect, actualString)
 	}
 }
 
 func ExpectNear(t *testing.T, expect float64, actual float64, acc float64) {
 	if math.Abs(expect-actual) > acc {
-		t.Errorf("期待值=\"%s\", 实际=\"%s\"", expect, actual)
+		t.Errorf("Expected value=\"%s\", Actual value=\"%s\"", expect, actual)
 	}
 }
