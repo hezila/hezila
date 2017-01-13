49ba58ff83e8a4ec70d321a8445624554e05ad73
Felix Wang <wangfelix87@gmail.com>
2017-01-13 21:03:22 +0800

    revise doc.go

--- math/doc.go
+++ math/doc.go
@@ -1,7 +1,6 @@
-// Copyright 2016 Feng Wang. All rights reserved.
+// Copyright (c) 2016 Hezila Technologies, Inc.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 
-// Package math implement matrix package. It defines sparse, dense matrix.
 package math
